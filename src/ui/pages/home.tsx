3b4f589b0d1b3b5690dac03072852306d57b350f
Jeffrey Gordon <jeff@jdgwf.com>
2022-06-08 08:29:56 -0500

    News update

--- src/ui/pages/home.tsx
+++ src/ui/pages/home.tsx
@@ -86,10 +86,21 @@ export default class Home extends React.Component<IHomeProps, IHomeState> {
             >
                   <ul className="news">
                   <li>
-                    <p>
-                      <strong>2022 April 25</strong> - Intro 'mechs are nearly 100% matching across the board via SSW imports. Allocating Hatchets are the only import erros. All C-Bill and BV2 costs are correct.
-                    </p>
+                    <h4>
+                      <strong>2022 June 8</strong> - MUL Elemental and Infantry Size fixes &amp; Pilot Abilities
+                    </h4>
+                    <h5>Elemental Unit Sizes</h5>
+                    <p>The Master Unit List has split up Battle Armor unit sizes into larger and smaller chunks for those looking for over and under sized units. This took effect earlier this week and instantly fixed our <a href="https://github.com/jdgwf/battletech-tools/issues/13" target="github">Issue #13</a></p>
 
+                    <h5>Alpha Strike Pilot Cards</h5>
+                    <p>I've added initial support for Pilot Cards in the Alpha Strike roster. The pilot card is selected in the Unit Edit screen (where you'd change the pilot skill or unit name). The pilot card will be printed next to the 'mech card, listed beneath the mech listing, and a clickable link in the In-Play screen. It's noted on the Unit Card in green below the name for ease of recognition. The PV is listed on the card, but added to the unit cost on the card just in case it's not used during the game. The Pilot card <strong>is</strong> added to the Group and Force totals, though.</p>
+                  </li>
+                  <li>
+                    <h4>
+                      <strong>2022 April 25</strong>
+                    </h4>
+
+                    <p>Intro 'mechs are nearly 100% matching across the board via SSW imports. Allocating Hatchets are the only import errors. All C-Bill and BV2 costs are correct.</p>
                     <h5>CBT Mech Creator</h5>
                     <p>The 'mech creator now has the ability to split criticals.</p>
                     <ol>
@@ -105,54 +116,64 @@ export default class Home extends React.Component<IHomeProps, IHomeState> {
 
                   </li>
                   <li>
-                    <p>
-                      <strong>2022 April 16</strong> - Solaris Skunk Werks data for 3039TRO should be 100% good to go.  My internal <Link to="ssw-sanity-check">Sanity Test</Link> page has all the calculations for c-bills and bv2 matching 100% for all 213 mechs for the 3039 TRO. This wasn't easy, but it sure helped clean up my BV2 calculator. On slower computers or phones, this may take a few of seconds to load as it imports each of the SSW files, calculates the BV2 and C-Bill cost, then compares it to the XML file's stated BV2 and C-Bill costs.
-                    </p>
+                    <h4>
+                      <strong>2022 April 16</strong>
+                    </h4>
+                    <p>Solaris Skunk Werks data for 3039TRO should be 100% good to go.  My internal <Link to="ssw-sanity-check">Sanity Test</Link> page has all the calculations for c-bills and bv2 matching 100% for all 213 mechs for the 3039 TRO. This wasn't easy, but it sure helped clean up my BV2 calculator. On slower computers or phones, this may take a few of seconds to load as it imports each of the SSW files, calculates the BV2 and C-Bill cost, then compares it to the XML file's stated BV2 and C-Bill costs.</p>
                     <p>Needless to say this is a huge step in the direction of accuracy for this app.</p>
 
                     <h5>Addendum</h5>
                     <p>The handful of 3050U Inner Sphere 'mechs have been imported, and are also matching their SSW values</p>
                   </li>
                   <li>
-                    <p><strong>2022 April 14</strong> - I've added a tentative <Link to="classic-battletech/mech-creator/imports">importer</Link> for the Solar Skunk Werk's Master File format. I'm having relative good success with Inner Sphere mechs (<del>without jump jets, for now, perhaps tomorrow or Saturday</del> nah, already added it: Stingers, Phoenix Hawks, and Wasps all work perfectly!).</p>
+                    <h4><strong>2022 April 14</strong></h4>
+                    <p>I've added a tentative <Link to="classic-battletech/mech-creator/imports">importer</Link> for the Solar Skunk Werk's Master File format. I'm having relative good success with Inner Sphere mechs (<del>without jump jets, for now, perhaps tomorrow or Saturday</del> nah, already added it: Stingers, Phoenix Hawks, and Wasps all work perfectly!).</p>
                     <p>The Repo is here: <a href="https://github.com/Solaris-Skunk-Werks/SSW-Master" target="ssw">Solaris Skunk Werks Master Data Repo</a>, and the direct download is here: <a href="https://github.com/Solaris-Skunk-Werks/solarisskunkwerks/releases" target="ssw">SSW-Master.zip is the file you want</a></p>
                   </li>
                   <li>
-                    <p><strong>2022 April 9</strong> - I've activated the "bigger menu" for folks who haven't checked the "Show Developer/Work In Progress Menu" option in settings. This means that the <Link to="classic-battletech/roster">CBT Roster</Link> is now visible to search engines and normal folk.</p>
+                    <h4><strong>2022 April 9</strong></h4>
+                    <p>I've activated the "bigger menu" for folks who haven't checked the "Show Developer/Work In Progress Menu" option in settings. This means that the <Link to="classic-battletech/roster">CBT Roster</Link> is now visible to search engines and normal folk.</p>
                     <p>I've also added an intro blurb to the home page explaining the purpose of this web app.</p>
                   </li>
                   <li>
-                      <p><strong>2022 April 8</strong> - The CBT Roster In-Play Section is coming along, albeit slower than I'd like, but there are a lot more moving parts compared to the Alpha Strike Roster.</p>
+                      <h4><strong>2022 April 8</strong></h4>
+                      <p>The CBT Roster In-Play Section is coming along, albeit slower than I'd like, but there are a lot more moving parts compared to the Alpha Strike Roster.</p>
                       <p>Right now what works: Movement, Target Selection, Weapon Target Assignments (along with GATOR calculations), and Fire Control Resolving marks (as well as cluster hit tracking)</p>
                       <p>Coming Soon (hopefully this weekend, health dependent?): Heat Allocation at end of turn, Damage Entry box (which will assign each damage per location auto-magically, and let you know the crits as you enter the damage, and Critical Damage tracking.</p>
                       <p>Right now you should be able to technically play without all the premium functions (keeping track of heat on your own, and clicking each damage box)</p>
                       <p>This is the very start of this in-play sheet. I'm sure once I get going there'll be more Quality of Life features coming (such as Ammo Explosion calculations)</p>
                     </li>
                     <li>
-                      <p><strong>2022 April 3</strong> - Work on the <Link to="classic-battletech/roster">CBT Roster</Link> has started. If you want to see it in the menu, go to <Link to="settings">Settings</Link> then click on "Show Developer/Work In Progress Menu"</p>
+                      <h4><strong>2022 April 3</strong></h4>
+                      <p>Work on the <Link to="classic-battletech/roster">CBT Roster</Link> has started. If you want to see it in the menu, go to <Link to="settings">Settings</Link> then click on "Show Developer/Work In Progress Menu"</p>
                       <p>The basic rostering functions should work, but the Print and Play buttons don't have a URL endpoint yet. I'll have the Print completed first, but the Play button will likely take me the better part of a week to code as I'll need to code the Electronic Character Sheet functions both into the SVG Record Sheet and in the BattleMech class object </p>
                       <p>Ideally all you should need is your tablet for quick and easy lance management during your games. This may take a while for me to iron out the in-play workflow</p>
                       <p className="text-center"><strong>Update</strong>: Print Roster function seems to be working great already!</p>
                     </li>
                   <li>
-                      <p><strong>2022 April 2</strong> - Added Rotary ACs for Inner Sphere. I think this closes out the Ballistic Weapons for that tech. Shows up /shot on record sheet too.</p>
+                      <h4><strong>2022 April 2</strong></h4>
+                      <p>Added Rotary ACs for Inner Sphere. I think this closes out the Ballistic Weapons for that tech. Shows up /shot on record sheet too.</p>
 
                     </li>
                     <li>
-                      <p><strong>2022 Mar 13</strong> - The <Link to="settings">Settings</Link> page now includes some total backup/restore functionality. Some care has been made to be sure that if you have any new items on the restore-to device, that the data won't be overwritten. Work still progresses on that safety. You should now have all you need to sync (manually) across devices until I find some syncing solution which is compatible with GitHub pages.</p>
+                      <h4><strong>2022 Mar 13</strong></h4>
+                      <p>The <Link to="settings">Settings</Link> page now includes some total backup/restore functionality. Some care has been made to be sure that if you have any new items on the restore-to device, that the data won't be overwritten. Work still progresses on that safety. You should now have all you need to sync (manually) across devices until I find some syncing solution which is compatible with GitHub pages.</p>
 
                     </li>
                     <li>
-                      <p><strong>2022 Mar 11</strong> - I've started reworking the <Link to="alpha-strike/roster">Alpha Strike Roster</Link> interface. Adding units to the current force is now on a button. Later I might change the main screen so that all the Current Units show up on top and favorite units will be at the bottom. Right now this cleans uop the page significantly, but I'm still not happy with it.</p>
+                      <h4><strong>2022 Mar 11</strong></h4>
+                      <p>I've started reworking the <Link to="alpha-strike/roster">Alpha Strike Roster</Link> interface. Adding units to the current force is now on a button. Later I might change the main screen so that all the Current Units show up on top and favorite units will be at the bottom. Right now this cleans uop the page significantly, but I'm still not happy with it.</p>
 
                     </li>
                     <li>
-                      <p><strong>2022 Feb 23</strong> - Added a Print function to your Alpha Strike Rosters (finally!?!?!).</p>
+                      <h4><strong>2022 Feb 23</strong></h4>
+                      <p>Added a Print function to your Alpha Strike Rosters (finally!?!?!).</p>
                       <p>I might be working on Google Account data syncing this afternoon, although Google still gives me the heebie-jeebies. First, however, I want to finish the internal <Link to="equipment-editor">Equipment Editor</Link> so I can get the rest of the Clan and Inner Sphere weapons ready for the BattleMech creator.</p>
 
                     </li>
                     <li>
-                      <p><strong>2022 Feb 18</strong> - Since there's still a pretty big gap in Alpha Strike Force creation and BattleMech creation on mobile devices, I've decided to reopen this app again. The official App is great, albeit slow, and doesn't do much for Alpha Strike folks.</p>
+                      <h4><strong>2022 Feb 18</strong></h4>
+                      <p>Since there's still a pretty big gap in Alpha Strike Force creation and BattleMech creation on mobile devices, I've decided to reopen this app again. The official App is great, albeit slow, and doesn't do much for Alpha Strike folks.</p>
 
                     </li>
                   </ul>
